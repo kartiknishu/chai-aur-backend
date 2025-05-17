85889b04d7ca26e930a0128fae4da0976b5f9aca
Kartiknishu <kartiknishu2002@gmail.com>
2025-05-18 03:54:34 +0530

    added first controller

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (handler)=>{
     return(req, res, next)=>{
-        Promise.resolve(handler(req,res,next)).catch((err)=>next(err))
+        Promise.resolve(handler(req,res,next)).catch((err)=>{next(err); console.log("error in handler")})
     }
 }
 
